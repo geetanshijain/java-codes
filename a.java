d263d3f710633d961e989a4a08e985629d1e4a9d
Geetanshi Jain <gungungarg6125@gmail.com>
2026-04-16 13:52:24 +0530

    day 1 code

---
+++ a.java
@@ -0,0 +1,12 @@
+public class a {
+    public static void main(String[] args) {
+        
+        int sum=0;
+        for(int i=1;i<=5;i++){
+           sum += i*i;}
+            System.out.println(sum);
+        
+        
+    }
+}
+    
